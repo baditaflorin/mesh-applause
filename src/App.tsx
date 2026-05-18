7954975526d626b590dbdda15b17b8ea962762fc
Florin Badita-Nistor <baditaflorin@gmail.com>
2026-05-18 19:57:18 +0300

    feat: add InviteShareButton retrofit + mesh-common styles bridge + vitest dedupe

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { ApplauseWall } from "./features/applause/ApplauseWall";
 import { SettingsDrawer } from "./features/settings/SettingsDrawer";
 import { appConfig } from "./shared/config";
+import { InviteShareButton } from "@baditaflorin/mesh-common";
 
 const STORAGE = {
   room: `${appConfig.storagePrefix}:room`,
@@ -28,6 +29,7 @@ export function App() {
     <div className="app-root">
       <ApplauseWall roomId={roomId} myName={myName} />
 
+      <InviteShareButton appName={appConfig.appName} roomId={roomId} />
       <button
         type="button"
         className="settings-fab"
